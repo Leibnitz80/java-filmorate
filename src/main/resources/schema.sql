7f3d552ef56a1552f10c9b45b7293279be4aa533
Dmitry Lebedev <leibnitz@mail.ru>
2023-03-16 22:52:21 +0300

    Sprint 11 HotFix#1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,3 @@
-DROP TABLE Films;
-DROP TABLE Mpa;
-DROP TABLE Likes;
-DROP TABLE Users;
-DROP TABLE Genres;
-DROP TABLE Genres_Relation;
-DROP TABLE Friendship;
-
 CREATE TABLE IF NOT EXISTS Films(film_id int auto_increment,
 				   name varchar(160),
 				   description varchar(255),
