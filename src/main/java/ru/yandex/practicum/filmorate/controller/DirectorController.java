54dd7ffe04411b0747f1a99d6cfee97e752182f5
Sergei <starodumovtmt@yandex.ru>
2023-04-06 23:45:17 +0500

    fix: привести к общему виду текст log

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -18,31 +18,31 @@ public class DirectorController {
 
     @GetMapping("{id}")
     public Director getDirectorById(@PathVariable Integer id) {
-        log.info("Запрос: GET getDirectorById {}", id);
+        log.info("Запрос для Director: GET getDirectorById {}", id);
         return directorService.getDirectorById(id);
     }
 
     @DeleteMapping("{id}")
     public void deleteDirector(@PathVariable Integer id) {
-        log.info("Запрос: DELETE {} deleteDirector", id);
+        log.info("Запрос для Director: DELETE {} deleteDirector", id);
         directorService.deleteDirector(id);
     }
 
     @GetMapping()
     public List<Director> getDirectors() {
-        log.info("Запрос: GET getDirectors");
+        log.info("Запрос для Director: GET getDirectors");
         return directorService.getDirectors();
     }
 
     @PostMapping
     public Director addDirector(@Valid @RequestBody Director director) {
-        log.info("Запрос: POST addDirector {}", director);
+        log.info("Запрос для Director: POST addDirector {}", director);
         return directorService.addDirector(director);
     }
 
     @PutMapping
     public Director updateDirector(@Valid @RequestBody Director director) {
-        log.info("Запрос: PUT {} updateDirector", director);
+        log.info("Запрос для Director: PUT {} updateDirector", director);
         return directorService.updateDirector(director);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -15,27 +15,27 @@ public class DirectorService {
     private final DirectorStorage directorStorage;
 
     public List<Director> getDirectors() {
-        log.info("Запрос: GET getDirectors обработан успешно");
+        log.info("Запрос для Director: GET getDirectors обработан успешно");
         return directorStorage.getDirectors();
     }
 
     public Director addDirector(Director director) {
-        log.info("Запрос: POST addDirector {} обработан успешно", director);
+        log.info("Запрос для Director: POST addDirector {} обработан успешно", director);
         return directorStorage.addDirector(director);
     }
 
     public Director updateDirector(Director director) {
-        log.info("Запрос: PUT {} updateDirector обработан успешно", director);
+        log.info("Запрос для Director: PUT {} updateDirector обработан успешно", director);
         return directorStorage.updateDirector(director);
     }
 
     public void deleteDirector(Integer id) {
-        log.info("Запрос: DELETE {} deleteDirector обработан успешно", id);
+        log.info("Запрос для Director: DELETE {} deleteDirector обработан успешно", id);
         directorStorage.deleteDirector(id);
     }
 
     public Director getDirectorById(Integer id) {
-        log.info("Запрос: GET getDirectorById {} обработан успешно", id);
+        log.info("Запрос для Director: GET getDirectorById {} обработан успешно", id);
         return directorStorage.getDirectorById(id);
     }
 }
