cf3c70af39f5676124162080c6a6693bc475f90c
Dmitry Lebedev <leibnitz@mail.ru>
2023-04-06 18:48:44 +0300

    Gruppen project HotFix#1

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -14,7 +14,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
